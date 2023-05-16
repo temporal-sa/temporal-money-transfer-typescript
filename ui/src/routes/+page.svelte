0027575c112c9dc78a29e3696dcea5d89ba5faca
Steve Androulakis <steve.androulakis@gmail.com>
2023-05-16 16:21:39 -0700

    money transfer form

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -4,6 +4,18 @@
   import Badge from "@temporalio/ui/holocene/badge.svelte"
   import Loading from "@temporalio/ui/holocene/loading.svelte"
   import PageTitle from "@temporalio/ui/components/page-title.svelte"
+	import Button from '@temporalio/ui/holocene/button.svelte';
+
+	let fromAccount = '';
+	let toAccount = '';
+	let amount = 0;
+	const fromAccounts = ['Checking', 'Savings'];
+  const toAccounts = ['Jordan Morris', 'Raúl Ruidíaz', 'Iván Rancic', 'Fredy Montero'];
+
+	function transferMoney() {
+		// Add your money transfer logic here
+		alert(`Transferring $${amount} from ${fromAccount} to ${toAccount}.`);
+	}
 
   let seconds = 0;
 
@@ -27,20 +39,34 @@
   }
 </script>
 
-<PageTitle title="Temporal SvelteKit Starter" url={$page.url.href} />
-<section class="flex flex-col gap-8 items-center justify-center w-full h-screen">
+<PageTitle title="Money Transfer App" url={$page.url.href} />
+<section class="flex flex-col gap-8 items-center">
 	<Loading title="" />
-	<h1 class="text-2xl flex items-center gap-1">Welcome to Temporal SvelteKit Starter!</h1>
-	<div class="bg-gray-100 p-4 w-auto flex flex-col gap-2 items-center text-lg">
-		<p class="flex gap-1">
-			Project includes the <Badge type="running">@temporalio/ui</Badge> package and <Badge
-				type="warning">tailwind</Badge
-			> css
-		</p>
-    <div>{seconds}</div>
-    <button on:click={resetTimer}>Reset Timer</button>
+	<h2 class="text-2xl flex items-center gap-1">Transfer Money</h2>
+	<div class="w-1/2 border-2 border-gray-200 p-4 rounded-md">
+		<label for="from-account" class="block text-sm font-medium text-gray-700">From Account</label>
+		<select id="from-account" bind:value={fromAccount} class="mt-1 block w-full text-2xl">
+			{#each fromAccounts as account (account)}
+				<option>{account}</option>
+			{/each}
+		</select>
 	</div>
-	<a href="https://github.com/temporalio/ui" target="_blank" class="text-sm text-blue-700 underline"
-		>Temporal UI Github</a
-	>
+	<div class="w-1/2 border-2 border-gray-200 p-4 rounded-md">
+		<label for="to-account" class="block text-sm font-medium text-gray-700">To Account</label>
+		<select id="to-account" bind:value={toAccount} class="mt-1 block w-full text-2xl">
+			{#each toAccounts as account (account)}
+				<option>{account}</option>
+			{/each}
+		</select>
+	</div>
+	<div class="w-1/2 border-2 border-gray-200 p-4 rounded-md">
+		<label for="amount" class="block text-sm font-medium text-gray-700">Amount (In USD)</label>
+		<input id="amount" type="number" bind:value={amount} class="mt-1 block w-full text-4xl"/>
+	</div>
+	<div class="">
+		<Button on:click={transferMoney}>Transfer</Button>
+	</div>
+
+  <div>{seconds}</div>
+  <button on:click={resetTimer}>Reset Timer</button>
 </section>
